72dc08244b55098b9314875b16f44228e29de5a8
Jeremy <je7remy@gmail.com>
2024-02-14 21:56:10 -0400

    Menu para usuarios ADM

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -11,12 +11,10 @@ export class NavbarComponent implements OnInit {
 
 
 
-  menu_adm_param = [];
-
-  menu_adm_proceso = [];
-
-
-  menu_adm_reporte = [];
+  menu_adm_proceso: any[] = [];
+  menu_adm_param: any[] = [];
+  menu_adm_reporte: any[] = [];
+  
 
 
  login_data:any;
@@ -39,17 +37,17 @@ export class NavbarComponent implements OnInit {
 
 if (this.tipo_usuario =="ADM"){
      this.menu_adm_param = [
-//{ name: "Registro de usuario", url: "/dashboard/usuario", icono: "nav-icon fas fa-book" }, 
+{ name: "Registro de usuario", url: "/dashboard/usuario", icono: "nav-icon fas fa-book" }, 
   ];
   
     this.menu_adm_proceso = [
-    //  { name: "Ingresos/Egresos", url: "/dashboard/ingresos_egresos", icono: "nav-icon fas fa-book" },
+      { name: "Ingresos/Egresos", url: "/dashboard/ingresos_egresos", icono: "nav-icon fas fa-book" },
 
     ];
   
   
     this.menu_adm_reporte = [
-     // { name: "Egresos/Ingresos", url: "/dashboard/repcitas", icono: "nav-icon fas fa-file" },
+      { name: "Egresos/Ingresos", url: "/dashboard/repcitas", icono: "nav-icon fas fa-file" },
     ];
   
 
@@ -57,18 +55,26 @@ if (this.tipo_usuario =="ADM"){
 
     
     this.menu_adm_param = [
-
-   //{ name: "Registro de cie10", url: "/dashboard/cie10", icono: "nav-icon fas fa-book" },
-
+   { name: "Registro de Empresa", url: "/dashboard/citas", icono: "nav-icon fas fa-th" },
+   { name: "Registro de Sucursal", url: "/dashboard/citas", icono: "nav-icon fas fa-th" },
+   { name: "Registro de Vehiculos", url: "/dashboard/citas", icono: "nav-icon fas fa-th" },
+   { name: "Registro de Clientes", url: "/dashboard/citas", icono: "nav-icon fas fa-th" },
+   { name: "Registro de Usuario", url: "/dashboard/citas", icono: "nav-icon fas fa-th" },
  ];
 
  this.menu_adm_proceso = [
- //  { name: "Citas", url: "/dashboard/citas", icono: "nav-icon fas fa-th" },
+  { name: "Gastos", url: "/dashboard/cie10", icono: "nav-icon fas fa-book" },
+  { name: "Reservacion", url: "/dashboard/cie10", icono: "nav-icon fas fa-book" },
+  { name: "Factura", url: "/dashboard/cie10", icono: "nav-icon fas fa-book" },
+  { name: "Reparaciones", url: "/dashboard/cie10", icono: "nav-icon fas fa-book" },
  ];
 
 
  this.menu_adm_reporte = [
- //  { name: "Reporte de Citas", url: "/dashboard/repcitas", icono: "nav-icon fas fa-file" },
+   { name: "Egresos/Ingresos", url: "/dashboard/repcitas", icono: "nav-icon fas fa-file" },
+   { name: "Facturas", url: "/dashboard/repcitas", icono: "nav-icon fas fa-file" },
+   { name: "Clientes", url: "/dashboard/repcitas", icono: "nav-icon fas fa-file" },
+   { name: "Vehiculos Pendientes", url: "/dashboard/repcitas", icono: "nav-icon fas fa-file" },
 
  ];
    }
